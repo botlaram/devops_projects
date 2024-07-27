110a6a0cd5b3b98ddb1443ce4f694002806ceaac
bolta04 <successisonlyfcukingoption@gmail.com>
2024-07-27 20:08:15 +0530

    src and test dir

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,31 @@
 import nox
 
+DEFAULT_TEST_DIRECTORIES=['./tests']
+
 @nox.session
 def run_script(session):
+    """run main"""
     # Install dependencies
     session.install('-r', 'requirements.txt')
-    
+
+    # Run the scrip
+    session.chdir("src")
+
     # Run main.py
     session.run('python', 'main.py')
 
 @nox.session
 def tests(session):
+    """run test cases"""
     # Install dependencies
     session.install('-r', 'requirements.txt')
+    session.env["FORCE_COLOUR"] = "1"
     
+    # pass test case dir as args to pytest
+    if session.posargs:
+        args=session.posargs
+    else:
+        args=DEFAULT_TEST_DIRECTORIES
+  
     # Run tests
-    session.run('pytest','test.py')
+    session.run('python', '-m', 'pytest')
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import *
+from src.main import add, subtract, multiply
 
 class TestMainFunctions(unittest.TestCase):
     
